9f716170c0d018208272f987fd4789dc7a4508bd
Mr.Felix <104478664+ChuckFelix765@users.noreply.github.com>
2024-10-09 18:47:46 -0300

    Add files via upload

--- Comentario.java
+++ Comentario.java
@@ -6,10 +6,13 @@ public class Comentario extends AFD {
 	@Override
 	public Token evaluate (CharacterIterator code){
         if(code.current() == '#'){
+            code.next();
             while(code.current() != '#'){
                 code.next();
             }
+            code.next();
             return new Token("COMT","#");
         }
+        return null;
 	}
 }
