b735cce1010b140f59d6a145cf302c1253431512
Alan Suga <alandaiki9@gmail.com>
2024-11-10 22:02:36 -0300

    commit agor agrinha

--- Code.py
+++ Code.py
@@ -1,29 +1,14 @@
+def fore(a):
+	for x in range(0, 10):
+		print("Valor atual de a: ")
+		print("%d" %a)
+
+
+
 def main():
-	nome = str()
-	sobrenome = str()
-	idade = int()
-	diferenca = int(0)
-	print("Digite o seu nome: ")
-	nome = str(input()) 
-	print("Digite o seu sobrenome: ")
-	sobrenome = str(input()) 
-	print("Digite a sua idade: ")
-	idade = int(input()) 
-	if  idade<18:
-		print("MENOR DE IDADE\n")
-		while idade<18:
-			diferenca=diferenca+1
-			idade=idade+1
-
-		print("RETORNE EM TANTOS ANOS: ")
-		print("%d" %diferenca)
-
-	elif idade>=18:
-		print("MAIOR DE IDADE")
-
-	else :
-		print("ERRO")
-
+	a = int()
+	a = int(input()) 
+	fore(a)
 
 
 main()
